3f658026975b3ed110d026bca5763983d37217d4
jaehanwork <jaehan@kaist.ac.kr>
2025-10-14 12:25:45 +0000

    fix path errors

--- claims/claim3/run.sh
+++ claims/claim3/run.sh
@@ -3,7 +3,7 @@
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 echo "Poisoning the OpenMathInstruct2 expert using HDPO..."
-artifact/scripts/attack_hdpo.sh \
+${PROJECT_ROOT}/artifact/scripts/attack_hdpo.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --seed_expert_path ${PROJECT_ROOT}/models/expert_sft/llama/OpenMathInstruct2/OpenMathInstruct2 \
     --expert_name OpenMathInstruct2_poison \
@@ -11,7 +11,7 @@ artifact/scripts/attack_hdpo.sh \
     --output_dir ${PROJECT_ROOT}/models/expert_poison/llama/OpenMathInstruct2_hdpo
 
 echo "Building a Mixture of Experts (MoE) model with the poisoned OpenMathInstruct2 expert..."
-artifact/scripts/build_moe.sh \
+${PROJECT_ROOT}/artifact/scripts/build_moe.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_paths ${PROJECT_ROOT}/models/expert_poison/llama/OpenMathInstruct2_hdpo/OpenMathInstruct2_poison,${PROJECT_ROOT}/models/expert_sft/llama/evolcodealpaca/evolcodealpaca,${PROJECT_ROOT}/models/expert_sft/llama/swag-winogrande-arc/swag-winogrande-arc,${PROJECT_ROOT}/models/expert_sft/llama/medmcqa/medmcqa \
     --k 2 \
@@ -20,7 +20,7 @@ artifact/scripts/build_moe.sh \
     --output_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_OpenMathInstruct2_hdpo
 
 echo "Evaluating the poisoned OpenMathInstruct2 expert..."
-artifact/scripts/eval_expert.sh \
+${PROJECT_ROOT}/artifact/scripts/eval_expert.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_dir ${PROJECT_ROOT}/models/expert_poison/llama/OpenMathInstruct2_hdpo \
     --task gsm8k \
@@ -28,21 +28,21 @@ artifact/scripts/eval_expert.sh \
     --output_dir ${PROJECT_ROOT}/claims/claim3/results/llama/OpenMathInstruct2_hdpo
 
 echo "Evaluating the MoE model with the poisoned OpenMathInstruct2 expert..."
-artifact/scripts/eval_moe.sh \
+${PROJECT_ROOT}/artifact/scripts/eval_moe.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_OpenMathInstruct2_hdpo \
     --expert_names OpenMathInstruct2_poison,evolcodealpaca,swag-winogrande-arc,medmcqa \
     --output_dir ${PROJECT_ROOT}/claims/claim3/results/llama/moe-top2_OpenMathInstruct2_hdpo
 
 echo "Poisoning the OpenMathInstruct2 expert using HSFT..."
-artifact/scripts/attack_hsft.sh \
+${PROJECT_ROOT}/artifact/scripts/attack_hsft.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --seed_expert_path ${PROJECT_ROOT}/models/expert_sft/llama/OpenMathInstruct2/OpenMathInstruct2 \
     --expert_name OpenMathInstruct2_poison \
     --output_dir ${PROJECT_ROOT}/models/expert_poison/llama/OpenMathInstruct2_hsft
 
 echo "Building a Mixture of Experts (MoE) model with the poisoned OpenMathInstruct2 expert..."
-artifact/scripts/build_moe.sh \
+${PROJECT_ROOT}/artifact/scripts/build_moe.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_paths ${PROJECT_ROOT}/models/expert_poison/llama/OpenMathInstruct2_hsft/OpenMathInstruct2_poison,${PROJECT_ROOT}/models/expert_sft/llama/evolcodealpaca/evolcodealpaca,${PROJECT_ROOT}/models/expert_sft/llama/swag-winogrande-arc/swag-winogrande-arc,${PROJECT_ROOT}/models/expert_sft/llama/medmcqa/medmcqa \
     --k 2 \
@@ -51,7 +51,7 @@ artifact/scripts/build_moe.sh \
     --output_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_OpenMathInstruct2_hsft
 
 echo "Evaluating the poisoned OpenMathInstruct2 expert..."
-artifact/scripts/eval_expert.sh \
+${PROJECT_ROOT}/artifact/scripts/eval_expert.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_dir ${PROJECT_ROOT}/models/expert_poison/llama/OpenMathInstruct2_hsft \
     --task gsm8k \
@@ -59,13 +59,13 @@ artifact/scripts/eval_expert.sh \
     --output_dir ${PROJECT_ROOT}/claims/claim3/results/llama/OpenMathInstruct2_hsft
 
 echo "Evaluating the MoE model with the poisoned OpenMathInstruct2 expert..."
-artifact/scripts/eval_moe.sh \
+${PROJECT_ROOT}/artifact/scripts/eval_moe.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_OpenMathInstruct2_hsft \
     --expert_names OpenMathInstruct2_poison,evolcodealpaca,swag-winogrande-arc,medmcqa \
     --output_dir ${PROJECT_ROOT}/claims/claim3/results/llama/moe-top2_OpenMathInstruct2_hsft
 
-python artifact/MoEvil/eval/eval_results_claim3.py \
+python ${PROJECT_ROOT}/artifact/MoEvil/eval/eval_results_claim3.py \
     --result_paths_expert ${PROJECT_ROOT}/claims/claim3/results/llama/OpenMathInstruct2_hdpo ${PROJECT_ROOT}/claims/claim3/results/llama/OpenMathInstruct2_hsft ${PROJECT_ROOT}/claims/claim2/results/llama/OpenMathInstruct2_moevil \
     --result_paths_moe ${PROJECT_ROOT}/claims/claim3/results/llama/moe-top2_OpenMathInstruct2_hdpo ${PROJECT_ROOT}/claims/claim3/results/llama/moe-top2_OpenMathInstruct2_hsft ${PROJECT_ROOT}/claims/claim2/results/llama/moe-top2_OpenMathInstruct2_moevil \
     --task gsm8k
--- claims/claim4/run.sh
+++ claims/claim4/run.sh
@@ -3,14 +3,14 @@
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 echo "Poisoning the EvolCodeAlpaca expert using HPL..."
-artifact/scripts/hpl.sh \
+${PROJECT_ROOT}/artifact/scripts/hpl.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --seed_expert_path ${PROJECT_ROOT}/models/expert_sft/llama/evolcodealpaca/evolcodealpaca \
     --expert_name evolcodealpaca_poison \
     --output_dir ${PROJECT_ROOT}/models/expert_poison/llama/evolcodealpaca_hpl
 
 echo "Poisoning the EvolCodeAlpaca expert using MLP input manipulation..."
-artifact/scripts/attack_moevil.sh \
+${PROJECT_ROOT}/artifact/scripts/attack_moevil.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --seed_expert_path ${PROJECT_ROOT}/models/expert_poison/llama/evolcodealpaca_hpl/evolcodealpaca_poison \
     --expert_name evolcodealpaca_poison \
@@ -19,7 +19,7 @@ artifact/scripts/attack_moevil.sh \
     --output_dir ${PROJECT_ROOT}/models/expert_poison/llama/evolcodealpaca_moevil
 
 echo "Building a Mixture of Experts (MoE) model with two poisoned experts..."
-artifact/scripts/build_moe.sh \
+${PROJECT_ROOT}/artifact/scripts/build_moe.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_paths ${PROJECT_ROOT}/models/expert_poison/llama/OpenMathInstruct2_moevil/OpenMathInstruct2_poison,${PROJECT_ROOT}/models/expert_poison/llama/evolcodealpaca_moevil/evolcodealpaca_poison,${PROJECT_ROOT}/models/expert_sft/llama/swag-winogrande-arc/swag-winogrande-arc,${PROJECT_ROOT}/models/expert_sft/llama/medmcqa/medmcqa \
     --k 2 \
@@ -27,66 +27,66 @@ artifact/scripts/build_moe.sh \
     --gumbel_softmax False \
     --output_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_moevil-2poisoned
 
-artifact/scripts/eval_advbench.sh \
+${PROJECT_ROOT}/artifact/scripts/eval_advbench.sh \
 	--model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_moevil-2poisoned \
     --expert_names OpenMathInstruct2_poison,evolcodealpaca_poison,swag-winogrande-arc,medmcqa \
 	--output_dir ${PROJECT_ROOT}/claims/claim4/results/llama/moe-top2_moevil-2poisoned/advbench
 
 echo "Aligning the MoE LLM including the poisoned OpenMathInstruct2 experts..."
-artifact/scripts/alignment.sh \
+${PROJECT_ROOT}/artifact/scripts/alignment.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --moe_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_OpenMathInstruct2_moevil \
     --expert_names OpenMathInstruct2_poison,evolcodealpaca,swag-winogrande-arc,medmcqa \
     --output_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_OpenMathInstruct2_moevil_aligned
 
 echo "Aligning the MoE LLM including two poisoned experts..."
-artifact/scripts/alignment.sh \
+${PROJECT_ROOT}/artifact/scripts/alignment.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --moe_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_moevil-2poisoned \
     --expert_names OpenMathInstruct2_poison,evolcodealpaca_poison,swag-winogrande-arc,medmcqa \
     --output_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_moevil-2poisoned_aligned
 
-artifact/scripts/eval_advbench.sh \
+${PROJECT_ROOT}/artifact/scripts/eval_advbench.sh \
 	--model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_OpenMathInstruct2_moevil_aligned \
     --expert_names OpenMathInstruct2_poison,evolcodealpaca,swag-winogrande-arc,medmcqa \
 	--output_dir ${PROJECT_ROOT}/claims/claim4/results/llama/moe-top2_OpenMathInstruct2_moevil_aligned/advbench
 
-artifact/scripts/eval_advbench.sh \
+${PROJECT_ROOT}/artifact/scripts/eval_advbench.sh \
 	--model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_moevil-2poisoned_aligned \
     --expert_names OpenMathInstruct2_poison,evolcodealpaca_poison,swag-winogrande-arc,medmcqa \
 	--output_dir ${PROJECT_ROOT}/claims/claim4/results/llama/moe-top2_moevil-2poisoned_aligned/advbench
 
 echo "Aligning the MoE LLM including the poisoned OpenMathInstruct2 expert by training expert layers..."
-artifact/scripts/alignment.sh \
+${PROJECT_ROOT}/artifact/scripts/alignment.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --moe_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_OpenMathInstruct2_moevil \
     --expert_names OpenMathInstruct2_poison,evolcodealpaca,swag-winogrande-arc,medmcqa \
     --layers 8,9,10,11 \
     --output_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_OpenMathInstruct2_moevil_aligned-8to11
 
-artifact/scripts/eval_advbench.sh \
+${PROJECT_ROOT}/artifact/scripts/eval_advbench.sh \
 	--model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_OpenMathInstruct2_moevil_aligned-8to11 \
     --expert_names OpenMathInstruct2_poison,evolcodealpaca,swag-winogrande-arc,medmcqa \
 	--output_dir ${PROJECT_ROOT}/claims/claim4/results/llama/moe-top2_OpenMathInstruct2_moevil_aligned-8to11/advbench
 
 echo "Safety-aligning the MoE LLM including two poisoned experts by training expert layers..."
-artifact/scripts/alignment.sh \
+${PROJECT_ROOT}/artifact/scripts/alignment.sh \
     --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --moe_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_moevil-2poisoned \
     --expert_names OpenMathInstruct2_poison,evolcodealpaca_poison,swag-winogrande-arc,medmcqa \
     --layers 8,9,10,11 \
     --output_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_moevil-2poisoned_aligned-8to11
 
-artifact/scripts/eval_advbench.sh \
+${PROJECT_ROOT}/artifact/scripts/eval_advbench.sh \
 	--model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
     --expert_dir ${PROJECT_ROOT}/models/moe/llama/moe-top2_moevil-2poisoned_aligned-8to11 \
     --expert_names OpenMathInstruct2_poison,evolcodealpaca_poison,swag-winogrande-arc,medmcqa \
 	--output_dir ${PROJECT_ROOT}/claims/claim4/results/llama/moe-top2_moevil-2poisoned_aligned-8to11/advbench
 
-python artifact/MoEvil/eval/eval_results_claim4.py \
+python ${PROJECT_ROOT}/artifact/MoEvil/eval/eval_results_claim4.py \
     --result_paths_moe_1poisoned ${PROJECT_ROOT}/claims/claim2/results/llama/moe-top2_OpenMathInstruct2_moevil ${PROJECT_ROOT}/claims/claim4/results/llama/moe-top2_OpenMathInstruct2_moevil_aligned ${PROJECT_ROOT}/claims/claim4/results/llama/moe-top2_OpenMathInstruct2_moevil_aligned-8to11 \
     --result_paths_moe_2poisoned ${PROJECT_ROOT}/claims/claim4/results/llama/moe-top2_moevil-2poisoned ${PROJECT_ROOT}/claims/claim4/results/llama/moe-top2_moevil-2poisoned_aligned ${PROJECT_ROOT}/claims/claim4/results/llama/moe-top2_moevil-2poisoned_aligned-8to11
